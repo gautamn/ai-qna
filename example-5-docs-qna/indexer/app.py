342bebfb908d65458f0729eca5738fb1bb3fb24f
AndOr <andor@andor.mobi>
2023-05-06 11:52:11 +0530

    code for embedding creation

--- example-5-docs-qna/indexer/app.py
+++ example-5-docs-qna/indexer/app.py
@@ -4,6 +4,12 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.llms import OpenAI
+from openai.embeddings_utils import get_embedding
+import weaviate
+import os
+import openai
+
+
 
 
 def readPdfFile():
@@ -17,42 +23,60 @@ def readPdfFile():
     pdfFileObj.close()
     return text
 
-def indexPdf(text):
+def createCorpusOfChunks(text):
+  corpus = []
   # split into chunks
-    text_splitter = CharacterTextSplitter(
+  text_splitter = CharacterTextSplitter(
       separator="\n",
       chunk_size=1000,
       chunk_overlap=200,
       length_function=len
     )
-    chunks = text_splitter.split_text(text)
-    
-    # create embeddings
-    embeddings = OpenAIEmbeddings()
-    
-
-    retunr embeddings
-    
+  chunks = text_splitter.split_text(text)
+  for i in chunks:
+    newObj = {'content' : i}
+    corpus.append(newObj)
+  return corpus
 
 #insert the documents in weaviate
-def addDataInWeaviate(df):
+def addDataInWeaviate(corpus, openai_key, weaviate_url):
+    
+    client = weaviate.Client(
+      url = weaviate_url,  # Replace with your endpoint
+      additional_headers = {
+        "X-OpenAI-Api-Key": openai_key  # Replace with your inference API key
+      }
+    )
     client.batch.configure(batch_size=10)
     with client.batch as batch:
-        for index, row in df.iterrows():
-            text = row['content']
-            ebd = row['embedding']
+        for item in corpus:
+            text = item
+            #print(text)
+            print(text['content'])
+            print("**********************************************************************************************************")
+            ebd = generate_data_embeddings(text['content'])
             batch_data = {
-                "content": text
+                "content": text['content']
             }
-            batch.add_data_object(data_object=batch_data, class_name="NFTs", vector=ebd)
+            batch.add_data_object(data_object=batch_data, class_name="PDFDocument", vector=ebd)
 
     print("Data Added!")
 
+def generate_data_embeddings(item):
+  embedding = openai.Embedding.create(
+  input=item, model="text-embedding-ada-002")["data"][0]["embedding"]
+  len(embedding)
+  return embedding
 
 def main():
     load_dotenv()
+    openai.api_key = os.getenv('OPENAI_API_KEY')
+    weaviate_url = os.getenv('WEAVIATE_URL')
     text = readPdfFile()
-    print(text)
+    #print(text)
+    corpus = createCorpusOfChunks(text)
+    print(len(corpus))
+    addDataInWeaviate(corpus, openai.api_key, weaviate_url)
     
  
 if __name__ == '__main__':
